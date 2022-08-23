2c87b0b632a16e4de20387c5fd5ba2985d28a0b6
rebecatschiedel <rebecatcmg@gmail.com>
2022-08-23 10:26:57 -0700

    refactoring for mvc

---
+++ src/js/model.js
@@ -0,0 +1,32 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bce0d'
+      //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(data, data.data.recipe, recipe);
+  } catch (error) {
+    console.log(error);
+  }
+};
