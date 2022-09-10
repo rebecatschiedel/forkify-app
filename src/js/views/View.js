cf64773f341c642308af8794428b19d7c745dfde
rebecatschiedel <rebecatcmg@gmail.com>
2022-09-10 16:56:41 -0700

    Document the main function using jsdoc.app

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,14 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * jsdoc.app
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data  The data to be rendered (e.g. recipe)
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
+   * @returns{undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   */
   render(data, render = true) {
     this._data = data;
 
